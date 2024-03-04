6e1145fd612d6cd6c926b82953c9b8707c39973c
Socru ctr <129834786+Socru@users.noreply.github.com>
2024-03-04 17:41:02 -0600

    Add files via upload
    aun falla el nuevo juego

---
+++ Juego/js-curso-2-main/js-curso-2-main/app.js
@@ -0,0 +1,21 @@
+let mensaje = document.querySelector('h1');
+mensaje.innerHTML = 'Hora del desafio';
+function consoleclick(){
+    alert('console cliqueado');
+}
+
+function mensajeprompt(){
+    let ciudad = prompt('Escribe una ciudad de brazil', 'Rio de Janeiro');
+    alert("estuve en "+ciudad+" y me acorde de ti");
+}
+function alertaclick(){
+    alert(" yo amo js");
+}
+
+function mensajeprompt(){
+    let num1, num2, num3;
+    num1 = prompt('Escribe un numero');
+    num2 = prompt('Escribe otro numero');
+    (num3) = (parseInt( num1) + parseInt( num1) );
+    alert("la suma es "+num3);
+}
